942fc912b45c4ca15fca313975c1817801e63ced
hajungIm <89908402+ihj0303@users.noreply.github.com>
2023-11-22 17:06:09 +0900

    엔티티 설계, 매핑

---
+++ src/main/java/umc/workbook/domain/Member.java
@@ -0,0 +1,80 @@
+package umc.workbook.domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.workbook.domain.common.BaseEntity;
+import umc.workbook.domain.enums.Gender;
+import umc.workbook.domain.enums.MemberStatus;
+import umc.workbook.domain.enums.SocialType;
+import umc.workbook.domain.mapping.MemberAgree;
+import umc.workbook.domain.mapping.MemberMission;
+import umc.workbook.domain.mapping.MemberPrefer;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+    private Integer age;
+
+    @Column(nullable = false, length = 40)
+    private String address;
+
+    @Column(nullable = false, length = 40)
+    private String specAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private SocialType socialType;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    private Integer point;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberAgree> memberAgreeList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
